cf59f97f88373d6161a596c859a092a72058f8fb
jiangdeping <68100629@163.com>
2022-01-11 14:57:55 +0800

    update

--- test/test.py
+++ test/test.py
@@ -2,5 +2,7 @@ import configparser
 import os
 
 config = configparser.ConfigParser()
-config=config.read(os.path.join(os.path.abspath('..'), 'iselenium.ini'))
+config=config.read(os.path.join(os.path.abspath(''), 'iselenium.ini'))
+print(os.path.abspath('..'))
+print(os.path.join(os.path.abspath('..')))
 print(config)
